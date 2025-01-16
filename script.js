7eea9c1f27d33832c782591efe3738d909d2676c
Francis <167620859+francislame@users.noreply.github.com>
2025-01-15 19:16:56 -0600

    Update script.js
    Edited contents of alert and header.content

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Simple JavaScript to display a greeting message when the page loads
 window.onload = function() {
-    alert("Welcome to My First HTML Page!");
+    alert("Website template #1");
 };
 
 // Function to change the text content of the header
 function changeHeaderText() {
     const header = document.querySelector('header h1');
-    header.textContent = "Hello, Welcome to My Website!";
+    header.textContent = "Hello!";
 }
 
 // Add event listener to the header to change text when clicked
