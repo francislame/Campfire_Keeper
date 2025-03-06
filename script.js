9d0137c22ed38d2a934987908a096e01256f54fa
Francis <167620859+francislame@users.noreply.github.com>
2025-03-06 11:26:24 -0600

    Changed everything to suit Campfire Keeper instead of website template.

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
 // Simple JavaScript to display a greeting message when the page loads
 window.onload = function() {
-    alert("Website template #1");
-};
-
-// Function to change the text content of the header
-function changeHeaderText() {
-    const header = document.querySelector('header h1');
-    header.textContent = "Hello!";
+    alert("Welcome to Campfire Keeper!");
 }
-
-// Add event listener to the header to change text when clicked
-document.querySelector('header').addEventListener('click', changeHeaderText);
