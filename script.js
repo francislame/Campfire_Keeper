02e5195a5d57bcae650a3ea628c9f725712a86d6
Francis <167620859+francislame@users.noreply.github.com>
2025-03-06 12:10:26 -0600

    Added game-start functions.

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-// Simple JavaScript to display a greeting message when the page loads
+// Alert Message when the page loads
 window.onload = function() {
     alert("Welcome to Campfire Keeper!");
 }
+
+// game-start button
+const gameStartButton = document.getElementById('game-start-button');
+const gameStartAudio = document.getElementById('game-start-audio');
+// Event Listener for the game-start-button
+gameStartButton.addEventListener
+(
+    'click',
+    function() 
+        {
+        gameStartAudio.play()
+        }
+);
